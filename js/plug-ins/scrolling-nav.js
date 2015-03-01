d4e913a19abeb2e4d179c9cdc58458647155c4e5
Adam Parker <adam@theadamparker.com>
2015-02-28 19:50:40 -0500

    changed easing

--- js/plug-ins/scrolling-nav.js
+++ js/plug-ins/scrolling-nav.js
@@ -3,7 +3,7 @@ $(function() {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
-        }, 1000, 'easeOutExpo');
+        }, 1000, 'easeInOutExpo');
         event.preventDefault();
     });
 });
