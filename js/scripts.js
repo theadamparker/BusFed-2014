bfe873142f95b08e01fa528053f029f2c8f109a7
Adam Parker <adam@theadamparker.com>
2015-03-08 20:41:11 -0400

    removed selectors in scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // sticky elements
 
 $(document).ready(function () {
-  var top = $('.stick .col-1-3').offset().top - parseFloat($('.stick .col-1-3').css('marginTop').replace(/auto/, 0));
+  var top = $('.stick').offset().top - parseFloat($('.stick').css('marginTop').replace(/auto/, 0));
   $(window).scroll(function (event) {
     // what the y position of the scroll is
     var y = $(this).scrollTop();
@@ -9,10 +9,10 @@ $(document).ready(function () {
     // whether that's below the form
     if (y >= top) {
       // if so, ad the fixed class
-      $('.stick .col-1-3').addClass('fixed');
+      $('.col-1-3').addClass('fixed');
     } else {
       // otherwise remove it
-      $('.stick .col-1-3').removeClass('fixed');
+      $('col-1-3').removeClass('fixed');
     }
   });
 });
