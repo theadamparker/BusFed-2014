c1d15a00aa7e14738c73fd588663009ecbb21860
Adam Parker <adam@theadamparker.com>
2015-03-24 22:58:26 -0400

    Added Google charts

---
+++ js/chart.js
@@ -0,0 +1,32 @@
+// Load the Visualization API and the piechart package.
+google.load('visualization', '1.0', {'packages':['corechart']});
+
+// Set a callback to run when the Google Visualization API is loaded.
+google.setOnLoadCallback(drawChart);
+
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
+function drawChart() {
+  // Civic fund data
+  // Create the data table.
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Topping');
+  data.addColumn('number', 'Slices');
+  data.addRows([
+    ['Grants', 721600],
+    ['Contributions', 136936],
+    ['In-Kind Contributions', 5617],
+    ['Interest Income', 1025],
+    ['Other Revenue', 694]
+  ]);
+
+  // Set chart options
+  var options = {'title':'Bus Fed Revenue',
+                 'width':400,
+                 'height':300};
+
+  // Instantiate and draw our chart, passing in some options.
+  var chart = new google.visualization.PieChart(document.getElementById('chart_revenue'));
+  chart.draw(data, options);
+}
